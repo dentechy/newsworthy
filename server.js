ab52c229fe3497a5d49645b6a05a4689a794f644
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-09-05 20:18:42 -0400

    fixes dependent typo

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 //Defines dependencies
 var express = require("express");
 var mongoose = require("mongoose");
-var express = require("express");
+var cheerio = require("cheerio");
 var request = require("request");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+
+mongoose.Promise = Promise;
+
+var app = express();
 
 
